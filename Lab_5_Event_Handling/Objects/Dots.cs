74da6c8edc471eabd65dfe57c3602c1d6331953c
tasya132002@gmail.com <tasya132002@gmail.com>
2021-11-14 21:39:14 +0800

    Добавлены поля для максимальной координаты по оси х и по оси у добавлен мметод для изменения цвета

--- Lab_5_Event_Handling/Objects/Dots.cs
+++ Lab_5_Event_Handling/Objects/Dots.cs
@@ -1,8 +1,4 @@
 ﻿using System.Drawing;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Drawing.Drawing2D;
 using System;
 
@@ -11,23 +7,35 @@ namespace Lab_5_Event_Handling.Objects
     class Dots : BaseObject, IAction
     {
         Random randPosit = new Random();
-        float maxX;
-        float maxY;
+        private float maxX;
+        private float maxY;
         public Dots(float maxX, float maxY, float maxAngle) : base(maxX, maxY, maxAngle)
         {
-            wH = 20;
+            wObj=hObject = 20;
             this.maxX = maxX;
             this.maxY = maxY;
+            colorObj = Color.Yellow;
             Update();
         }
         public override void Render(Graphics g)
         {
-            g.FillEllipse(new SolidBrush(Color.Yellow), -wH / 2, -wH / 2, wH, wH);
+            g.FillEllipse(new SolidBrush(colorObj), -wObj / 2, -hObject / 2, wObj, hObject);
+        }
+        public override void reverseColor()
+        {
+            if (typeColor == Colors.DEFAULT)
+            {
+                colorObj = Color.Yellow;
+            }
+            else
+            {
+                base.reverseColor();
+            }
         }
         public override GraphicsPath GetGraphicsPath()
         {
             var path = base.GetGraphicsPath();
-            path.AddEllipse(-wH / 2, -wH / 2, wH, wH);
+            path.AddEllipse(-wObj / 2, -hObject / 2, wObj, hObject);
             return path;
         }
         public void Update(float playerX = -1, float playerY = -1)
@@ -35,8 +43,9 @@ namespace Lab_5_Event_Handling.Objects
 
           if(playerX == -1 && playerY == -1)
             {
-                X = randPosit.Next((int)(maxX - wH) + wH / 2);
-                Y = randPosit.Next((int)(maxY - wH) + wH / 2);
+                base.Overlap(this);
+                X = randPosit.Next((int)(maxX - wObj)) + wObj/2;
+                Y = randPosit.Next((int)(maxY - hObject)) + hObject / 2;
             }  
         }
     }
