e9469979a8ff6940f828b72a61603aec66fcb369
tasya132002@gmail.com <tasya132002@gmail.com>
2021-11-16 19:54:10 +0800

    В перерисовке(метод) теперь будут прасматриваться класс врага и союзника(т. есть если в массиве мы встретили объект класса врага происходила его обработка, аналогично с собзным классом)

--- Lab_5_Event_Handling/Form1.cs
+++ Lab_5_Event_Handling/Form1.cs
@@ -10,50 +10,55 @@ namespace Lab_5_Event_Handling
     public partial class Form1 : Form
     {
         List<BaseObject> objects = new();
-        //Dots[] dots;
-        Player player;
-        Marker marker;
-        MovingArea area;
+        Player player; //игрок
+        Marker marker; //маркер
+        MovingArea area; //черная область
+        CircleEnemy circleEnemy; //враг
         public Form1()
         {
             InitializeComponent();
             player = new Player(pictureBox1.Width / 2, pictureBox1.Height / 2, 0);
             area = new MovingArea(pictureBox1.Width / 2, pictureBox1.Height / 2, pictureBox1.Width, pictureBox1.Height);
-
-            player.onOverlap += (p, obj) =>
-            {
-                if(obj is Dots)
-                {
-                    Dots d = (Dots)obj;
-                    //richTextBox1.Text = $" x: {obj.getX()}\ny:  {obj.getY()}\n" + richTextBox1.Text;//595 399
-                }
+            circleEnemy = new CircleEnemy(pictureBox1.Width / 2, pictureBox1.Height / 2, pictureBox1.Width, pictureBox1.Height);
+            //player.onOverlap += (p, obj) =>
+            //{
+            //    if(obj is Dots)
+            //    {
+            //        Dots d = (Dots)obj;
+            //        //richTextBox1.Text = $" x: {obj.getX()}\ny:  {obj.getY()}\n" + richTextBox1.Text;//595 399
+            //    }
                     
-            };
+            //};
             player.OnOverlap += (obj) =>
-           {
-               if (obj is Marker) {
-                   objects.Remove(marker);
-                   marker = null; // и обнуляю маркер
+           { //Если игрок пересекся с объектом
+               if (obj is Marker) {        //Если маркер
+                   objects.Remove(marker); //Удалить маркер
+                   marker = null;          //и обнуляю маркер
                 }
-               else if (obj is Dots)
+               else if (obj is CircleAlien)//Если враг
                {
-                   ((Dots)obj).Update();
-                   Counter.Text = "Очки: " + player.getCountHit();
+                   ((CircleAlien)obj).Update(true); //Обновляем его размер или позицию
+                   Counter.Text = "Очки: " + player.getCountHit(); //Обновляем очки
                }
-               else if(obj is MovingArea)
+               else if(obj is MovingArea) //Если двигающаяся область
                {
                    richTextBox1.Text = $"peresecl x: {((MovingArea)obj).getX()}\ny:  {((MovingArea)obj).getY()}\n";
-               }
+               } //Получаем пересечение с объектами
            };
+
+            circleEnemy.onOverlapPlayer += (obj) =>
+            { //На пересечение игрока с врагом
+                Counter.Text = "Очки: " + player.getCountHit();
+            };
+
             marker = new Marker(pictureBox1.Width / 2 + 50, pictureBox1.Height / 2 + 50, 0);
             objects.Add(area);
 
-            objects.Add(new Dots(pictureBox1.Width, pictureBox1.Height, 0));
-            objects.Add(new Dots(pictureBox1.Width, pictureBox1.Height, 0));
-            objects.Add(player);
-            objects.Add(marker);
-
-            //objects. Add(new MyCircle(100, 100, 0));
+            objects.Add(new CircleAlien(pictureBox1.Width, pictureBox1.Height, 0)); //добавить союзный шарик
+            objects.Add(new CircleAlien(pictureBox1.Width, pictureBox1.Height, 0)); //добавить союзный шарик
+            objects.Add(circleEnemy); //добавить вражеский шарик
+            objects.Add(player);      //добавить игрока 
+            objects.Add(marker);      ////добавить маркер нажатия
 
         }
 
@@ -61,43 +66,52 @@ namespace Lab_5_Event_Handling
         {
             var g = e.Graphics;
 
-            g.Clear(Color.Black);
+            g.Clear(Color.FromArgb(61, 48, 103)); //Установитть цвет фона
 
-            if(marker != null)
-                player.Update(marker.getX(),marker.getY());
+            if(marker != null)                    //Если маркер существует
+                player.Update(marker.getX(),marker.getY()); //Обновить движение игрока до маркера
 
-            area.Update(pictureBox1.Width);
+            area.Update(pictureBox1.Width);    //Обновить движение черной области
+            circleEnemy.Update(false);         //Обновить расширение врага
+
             foreach (var obj in objects.ToList())
             {
-                // проверяю было ли пересечение с игроком
-                if (obj != player && player.Overlaps(obj, g))
+                if (obj is CircleAlien){              //Если это точка
+                    ((CircleAlien)obj).Update(false); //Обновить расшиерние точки
+                }
+               
+                if(obj != player && player.Overlaps(obj, g)) //Если это не игрок а объект, который пересекся с игроком,то
                 {
-                    player.Overlap(obj); // то есть игрок пересекся с объектом
+                    player.Overlap(obj); // Изменяем ифнормацию об объекте с которым пересеклись
+
+                    if (obj is CircleEnemy)
+                    { //Если мы пересеклись с врагом
+                        circleEnemy.Overlap(player); // Изменяем ифнормацию об объекте с которым пересеклись
+                        circleEnemy.Update(true);    //Изменить локацию и размер врага
+                    }
                 }
 
                 if (obj != area)
-                {
+                {//Если обект не черная область
                     bool itersect = false;
-                    if (area.Overlaps(obj, g))
+                    if (area.Overlaps(obj, g)) //Проверяем пересечение с объектом
                         itersect = true;
 
                     area.intersectUn(obj, itersect); // то есть area intersect с объектом
                 }
-                g.Transform = obj.getMatrix();
-                obj.Render(g);
+                g.Transform = obj.getMatrix();      //получить трансформирматор объектов
+                obj.Render(g);                      //Создать трансформированный объект
             }
-           // g.Transform = area.getMatrix();
-           // area.Render(g);
         }
         //public static int count = 0;
         private void timer1_Tick(object sender, EventArgs e)
         {  
-            pictureBox1.Invalidate();
+            pictureBox1.Invalidate(); //Перерисовать
         }
         private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
         {
             if (marker == null)
-            {
+            { //Содать новый маркер
                 marker = new Marker(0, 0, 0);
                 objects.Add(marker); // и главное не забыть пололжить в objects
             }
