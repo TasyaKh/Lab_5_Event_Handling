ff35eb0c1dec6263cae9c3f06e217350edf14445
tasya132002@gmail.com <tasya132002@gmail.com>
2021-11-16 19:52:31 +0800

    Будет суперклассом для других классов

--- Lab_5_Event_Handling/Objects/Dots.cs
+++ Lab_5_Event_Handling/Objects/Dots.cs
@@ -4,49 +4,30 @@ using System;
 
 namespace Lab_5_Event_Handling.Objects
 {
-    class Dots : BaseObject, IAction
+    class Dots : BaseObject
     {
-        Random randPosit = new Random();
-        private float maxX;
-        private float maxY;
-        public Dots(float maxX, float maxY, float maxAngle) : base(maxX, maxY, maxAngle)
+        protected Random rand = new Random();
+        protected float maxX; //Ширина сцены
+        protected float maxY; //Высота сцены
+
+        protected Dots(float maxX, float maxY, float maxAngle) : base(maxX, maxY, maxAngle)
         {
-            wObj=hObject = 20;
-            this.maxX = maxX;
-            this.maxY = maxY;
-            colorObj = Color.Yellow;
-            Update();
+            this.maxX = maxX;   //макс Ширина сцены
+            this.maxY = maxY;   //макс Высота сцены
         }
         public override void Render(Graphics g)
-        {
-            g.FillEllipse(new SolidBrush(colorObj), -wObj / 2, -hObject / 2, wObj, hObject);
-        }
-        public override void reverseColor()
-        {
-            if (typeColor == Colors.DEFAULT)
-            {
-                colorObj = Color.Yellow;
-            }
-            else
-            {
-                base.reverseColor();
-            }
+        { //Создаем круг
+            g.FillEllipse(new SolidBrush(colorObj), -wObj / 2, -hObj / 2, wObj, hObj);
         }
         public override GraphicsPath GetGraphicsPath()
         {
             var path = base.GetGraphicsPath();
-            path.AddEllipse(-wObj / 2, -hObject / 2, wObj, hObject);
+            path.AddEllipse(-wObj / 2, -hObj / 2, wObj, hObj);
             return path;
         }
-        public void Update(float playerX = -1, float playerY = -1)
-        {
-
-          if(playerX == -1 && playerY == -1)
-            {
-                base.Overlap(this);
-                X = randPosit.Next((int)(maxX - wObj)) + wObj/2;
-                Y = randPosit.Next((int)(maxY - hObject)) + hObject / 2;
-            }  
+        public virtual void Update(bool reverseLocation)
+        { //Будет выполеяться, когда нужно изменить положение или размер шарика
+          
         }
     }
 }
